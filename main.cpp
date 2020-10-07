d5506153f7ed891188a27c4906834962137d8035
Jesse Ruhl <jeruhl@chapman.edu>
2020-10-07 02:53:24 +0000

    created functions to put original matrix into a sort a new matrix with a border of zeros, which will add a mirrors border or torus border based on inner matrix

--- main.cpp
+++ main.cpp
@@ -7,15 +7,16 @@
 #include <array>
 using namespace std;
 //#include "GameOfLife.h"
+
 void nextGenerationClassic(int **a, int x, int y)
 {
-  //char[][] future = new char[numRows][numColumns];
-  //char future[x][y];
+  //new matrix for the next generation
   int **future = new int*[x];
   for (int i = 0; i < x; ++i)
   {
     future[i] = new int[y];
   }
+
   for(int l = 0; l < x; l++)
   {
     for (int m = 0; m < y; m++)
@@ -60,7 +61,106 @@ void nextGenerationClassic(int **a, int x, int y)
   }
 }
 
+void donutMatrix(int **a, int s, int t)
+{
+  //creates a new matrix, with 2 extra rows and columns
+  //fills the new outer border with zeros
+  int **donut = new int*[s + 2];
+  for (int i = 0; i <= s + 2; ++i)
+  {
+    donut[i] = new int[t + 2];
+  }
 
+  //copies the original matrix into the "inner matrix"
+
+  for (int i = 0; i <= s - 1; i++)
+  {
+    for(int j = 0; j <= t - 1; j++)
+    {
+      //donut[c][d] = a[c][d];
+      donut[i + 1][j + 1] = a[i][j];
+    }
+  }
+  //this segment goes through and makes the sides of the outer donut
+  //the oppsite of the inner border
+  for (int i = 1; i < t + 1; i++)
+  {
+    donut[0][i] = donut[s][i];
+    donut[s+1][i] = donut[1][i];
+  }
+  for (int j = 1; j < s + 1; j++)
+  {
+    donut[j][0] = donut[j][t];
+    donut[j][t + 1] = donut[j][1];
+  }
+  //this segment calculates the out borders of the Matrix
+  //makes those values the opposite corners of the inner matrix
+  donut[0][0] = donut[s][t];
+  donut[t + 1][s + 1] = donut [1][1];
+  donut[s + 1][0] = donut[1][t];
+  donut[0][t + 1] = donut[s][1];
+
+  cout << "Original Donut Matrix: " << endl;
+  for (int i = 0; i < s + 2; i++)
+  {
+    for (int j = 0; j < t + 2; j++)
+    {
+      cout << donut[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+void mirrorMatrix(int **a, int s, int t)
+{
+  //creates a new matrix, with 2 extra rows and columns
+  //fills the new outer border with zeros
+  int **mirror = new int*[s + 2];
+  for (int i = 0; i <= s + 2; ++i)
+  {
+    mirror[i] = new int[t + 2];
+  }
+
+  //copies the original matrix into the "inner matrix"
+
+  for (int i = 0; i <= s - 1; i++)
+  {
+    for(int j = 0; j <= t - 1; j++)
+    {
+      //donut[c][d] = a[c][d];
+      mirror[i + 1][j + 1] = a[i][j];
+    }
+  }
+  //mirrors the outer rows of the "inner matrix"
+  for (int i = 1; i < t + 1; i++)
+  {
+    mirror[0][i] = mirror[1][i];
+    mirror[s+1][i] = mirror[s][i];
+  }
+  //mirrors the outer columns of the "inner matrix"
+  for (int j = 1; j < s + 1; j++)
+  {
+    mirror[j][0] = mirror[j][1];
+    mirror[j][t + 1] = mirror[j][t];
+  }
+
+  //this segment calculates the out borders of the Matrix
+  //mirrors the corners of the inner matrix
+  mirror[0][0] = mirror[1][1];
+  mirror[t + 1][s + 1] = mirror [s][t];
+  mirror[s + 1][0] = mirror[s][1];
+  mirror[0][t + 1] = mirror[1][t];
+
+  cout << "Original Mirror Matrix: " << endl;
+  for (int i = 0; i < s + 2; i++)
+  {
+    for (int j = 0; j < t + 2; j++)
+    {
+      cout << mirror[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
 
 
 int main(/*int argc, char **argv*/)
@@ -114,13 +214,13 @@ int main(/*int argc, char **argv*/)
 
     srand((unsigned)time(NULL));
     //creates the matrix with the given input to form the size
-    //initializes the matrix with '-', representing empty cells
+    //initializes the matrix with 0, representing empty cells
     //will use the initialPopulationDensity to determine how many Xs will be randomly initialized
     for (int i = 0; i < numRows; i++)
     {
       for (int j = 0; j < numColumns; j++)
       {
-        //this line will initialize the matrix with '-', for empty cells
+        //this line will initialize the matrix with 0, for empty cells
         a[i][j] = 0;
 
       }
@@ -170,7 +270,10 @@ int main(/*int argc, char **argv*/)
       delete [] a[i];
     }
     */
+
     nextGenerationClassic(a, numRows, numColumns);
+    donutMatrix(a, numRows, numColumns);
+    mirrorMatrix(a, numRows, numColumns);
     //delete [] a;
   } //end of if statement where user inputs their own data
   //cout << endl;
