5d6a5fabedb5ede206b3f4788f45ef763252d728
Jesse Ruhl <jeruhl@chapman.edu>
2020-10-10 06:44:37 +0000

    Final upload. 'make realclean' is not working properly and gives find: paths must precede expression: - Try 'find --help' for more information. Makefile:24: recipe for target 'realclean' failed

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # CPSC350-Assignment2
+#Jesse Ruhl
+#2363770
+#Known Issues
+# 1. For an unknown reason, the "Output" mode
+#   has an issue where it will change some 0's to a '-' on the main screen
+#   and add a 1 next to the '-' within the same index.
+#   the program outputs this index to be full and places an  'X' .  
+#   However in the calculations to create the next generation, while it registers
+#   the index as a 0 still, it displays an 'X' although the 'X' does not affect the calculations
+# 2. make realclean is not working
+# after doing make all. using make realclean gives :
+#Try 'find --help' for more information.
+#Makefile:24: recipe for target 'realclean' failed
+#make: *** [realclean] Error 1
+
+#Works Cited
+# Bhargav, Skanda. GeeksForGeeks. https://ide.geeksforgeeks.org/iVA9hRlmD8
+# Software_Designer. "How to append file in c++ using fstream? [duplicate]". Stack Overflow. https://stackoverflow.com/questions/23615975/how-to-append-file-in-c-using-fstream
+# "C++ Sleep: How To Use The Sleep Function In C++ Programs". Software Testing Help. https://www.softwaretestinghelp.com/cpp-sleep/
+# adrei985 "How to add a border around a matrix?". StackOverflow. https://stackoverflow.com/questions/26952358/how-to-add-a-border-around-a-matrix
--- main.cpp
+++ main.cpp
@@ -5,12 +5,47 @@
 #include <iomanip>
 #include <cstdlib>
 #include <array>
+#include <string>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 using namespace std;
-//#include "GameOfLife.h"
 
-//thi function calculates the next geneartion for a classic matrix
-void nextGenerationClassic(int **a, int x, int y)
+//outputs the given grid out to an outfile
+void writeMessage(string message, string fileName)
 {
+  if (fileName != "")
+  {
+    ofstream outfile(fileName, fstream::app);
+    if (outfile.is_open())
+    {
+      outfile << message << endl;
+      outfile.close();
+    }
+  }
+  else
+  {
+    cout << message << endl;
+  }
+}
+
+//this function calculates the next geneartion for a classic matrix of original size
+int ** nextGenerationClassic(int **a, int x, int y, string fileName)
+{
+
+  cout << "input: " << endl;
+  for (int i = 0; i < x; i++)
+  {
+    for (int j = 0; j < y; j++)
+    {
+      cout << a[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+
   int **future = new int*[x];
   for (int i = 0; i < x; ++i)
   {
@@ -28,13 +63,16 @@ void nextGenerationClassic(int **a, int x, int y)
         {
           //checking boundaries of the matrix
           if((l+i <0) || (l+i > x-1) || (m+j <0) || (m+j > y))
-            //skips the nieghbor
+            //skips the neighbor
+            continue;
+          if (i == 0 & j == 0)
             continue;
           //increment counter for each live neighbor
           aliveNeighbors += a[l + i][m + j];
         }
+
       //remove counter
-      aliveNeighbors -= a[l][m];
+      //aliveNeighbors -= a[l][m];
       //determining whether the selected cell will live or die
       //alive cell is lonely and dies
       if (a[l][m] == 1 && aliveNeighbors <= 1)
@@ -50,12 +88,13 @@ void nextGenerationClassic(int **a, int x, int y)
         future[l][m] = 0;
       //alive cell is stable
       else if (a[l][m] == 1 && aliveNeighbors == 2)
-        future[l][m] = a[l][m];
-      //else if (a[l][m] == 0 && aliveNeighbors == 2)
-        //future[l][m] = a[l][m];
+      {
+        future[l][m] = 1;
+      }
     }
   }
-  //prints the updated classic cell
+
+
   cout << "Next Generation Classic: " << endl;
   for (int i = 0; i < x; i++)
   {
@@ -65,10 +104,27 @@ void nextGenerationClassic(int **a, int x, int y)
     }
     cout << endl;
   }
+  //prints the updated classic cell to outfile
+  string line= "";
+  for (int i = 0; i < x; i++)
+  {
+    for (int j = 0; j < y; j++)
+    {
+      //cout << futureAug[i][j] << " ";
+      if(future[i][j] == 0)
+        line +=  "-";
+      else
+        line += "X";
+    }
+    //cout << endl;
+    writeMessage(line,fileName);
+    line = "";
+  }
+  return future;
 }
 
 //calculates the next generation of a mirror or torus matrix
-void nextGenerationAugmented(int **a, int s, int t)
+int ** nextGenerationAugmented(int **a, int s, int t, string fileName)
 {
   //new matrix for the next generation
   int **futureAug = new int*[s + 2];
@@ -85,10 +141,12 @@ void nextGenerationAugmented(int **a, int s, int t)
       for (int i = -1; i <= 1; i++)
         for (int j = -1; j <= 1; j++)
         {
+          // skip the center cell
+          if (i == 0 & j == 0)
+            continue;
+
           aliveNeighbors += a[l + i][m + j];
         }
-
-      aliveNeighbors -= a[l][m];
 
       //cell is lonely and dies
       if (a[l][m] == 1 && aliveNeighbors <= 1)
@@ -104,25 +162,30 @@ void nextGenerationAugmented(int **a, int s, int t)
       //cell is stable
       else if (a[l][m] == 1 && aliveNeighbors == 2)
         futureAug[l][m] = a[l][m];
-      //else if (a[l][m] == 0 && aliveNeighbors == 2)
-        //future[l][m] = a[l][m];
     }
   }
-  //prints the updated augmented matrix
-  cout << "Next Generation Augmented: " << endl;
+  //outputs grid to text file by writing it as a string
+  string line= "";
   for (int i = 1; i <= s; i++)
   {
     for (int j = 1; j <= t; j++)
     {
-      cout << futureAug[i][j] << " ";
+      //cout << futureAug[i][j] << " ";
+      if(futureAug[i][j] == 0)
+        line +=  "-";
+      else
+        line += "X";
     }
-    cout << endl;
+    //cout << endl;
+    writeMessage(line,fileName);
+    line = "";
   }
+  return futureAug;
 }
-
-void donutMatrix(int **a, int s, int t)
+//creates a new matrix, with 2 extra rows and columns
+//calculates what the borders should now be filled with
+int ** donutMatrix(int **a, int s, int t, string fileName)
 {
-  //creates a new matrix, with 2 extra rows and columns
   //fills the new outer border with zeros
   int **donut = new int*[s + 2];
   for (int i = 0; i <= s + 2; ++i)
@@ -138,13 +201,15 @@ void donutMatrix(int **a, int s, int t)
       donut[i + 1][j + 1] = a[i][j];
     }
   }
-  //this segment goes through and makes the sides of the outer donut
+  //this segment goes through and makes the top and bottom of the outer donut
   //the oppsite of the inner border
   for (int i = 1; i < t + 1; i++)
   {
     donut[0][i] = donut[s][i];
     donut[s+1][i] = donut[1][i];
   }
+  //this segment goes through and makes the sides of the outer donut
+  //the oppsite of the inner border
   for (int j = 1; j < s + 1; j++)
   {
     donut[j][0] = donut[j][t];
@@ -158,21 +223,15 @@ void donutMatrix(int **a, int s, int t)
   donut[s + 1][0] = donut[1][t];
   //makes the "outer" upper right corner the "inner" lower left value
   donut[0][t + 1] = donut[s][1];
-  /*
-  cout << "Original Donut Matrix: " << endl;
-  for (int i = 0; i < s + 2; i++)
-  {
-    for (int j = 0; j < t + 2; j++)
-    {
-      cout << donut[i][j] << " ";
-    }
-    cout << endl;
-  }*/
   //passes the augmented matrix into the function to calulate its next gen
-  nextGenerationAugmented(donut, s, t);
+
+
+
+  return donut;
+  //nextGenerationAugmented(donut, s, t, fileName);
 }
 
-void mirrorMatrix(int **a, int s, int t)
+int ** mirrorMatrix(int **a, int s, int t, string fileName)
 {
   //creates a new matrix, with 2 extra rows and columns
   //fills the new outer border with zeros
@@ -212,142 +271,226 @@ void mirrorMatrix(int **a, int s, int t)
   mirror[s + 1][0] = mirror[s][1];
   //makes "outer" upper right corner the value of "inner" upper right corner
   mirror[0][t + 1] = mirror[1][t];
-  /*
-  cout << "Original Mirror Matrix: " << endl;
-  for (int i = 0; i < s + 2; i++)
-  {
-    for (int j = 0; j < t + 2; j++)
-    {
-      cout << mirror[i][j] << " ";
-    }
-    cout << endl;
-  }*/
-  nextGenerationAugmented(mirror, s, t);
-
+  return mirror;
+  //nextGenerationAugmented(mirror, s, t, fileName);
   //nextGenerationClassic(mirror, s, t);
-  //return mirror;
 }
+
+//converts a given string to be all upper case
+string toUpperFunction(string value)
+{
+  //char str[value.length()] = value;
+  string rtn = "";
+  for (int i = 0; i < value.length(); i++)
+  {
+    rtn += toupper(value[i]);
+  }
+  return rtn;
+}
+
+//tests if the cell will ever be full of 1's or 0's
+//if so it terminates the program
+int testIfStable(int **grid,int genCount, int numRows, int numColumns)
+{
+  int count;
+  //counts the sum of all the elements in the 2d array
+  for (int i = 0; i < numRows; i++)
+  {
+    for(int j = 0; j < numColumns; j++)
+    {
+      count += grid[i][j];
+    }
+  }
+  //if the sum is zero, it means that the grid comprises of all zeros
+  if (count == 0)
+  {
+    cout << "All cells within the matrix have died. It is stable." << endl;
+    return 1;
+  }
+  //if rows * columns is 1, then that means that all the elements are a 1
+  //due to 1^n
+  if (count == numRows * numColumns)
+  {
+    cout << "All cells within the matrix are alive. It is stable." << endl;
+    return 1;
+  }
+  return 0;
+}
+int testIfStableAug(int **grid,int genCount, int numRows, int numColumns)
+{
+  int count;
+  //counts the sum of all the elements in the 2d array
+  for (int i = 1; i <= numRows; i++)
+  {
+    for(int j = 1; j <= numColumns; j++)
+    {
+      count += grid[i][j];
+    }
+  }
+  //if the sum is zero, it means that the grid comprises of all zeros
+  if (count == 0)
+  {
+    cout << "All cells within the matrix have died. It is stable." << endl;
+    return 1;
+  }
+  //if rows * columns is 1, then that means that all the elements are a 1
+  //due to 1^n
+  if (count == numRows * numColumns)
+  {
+    cout << "All cells within the matrix are alive. It is stable." << endl;
+    return 1;
+  }
+  return 0;
+}
+
+
 
 int main(/*int argc, char **argv*/)
 {
+  string outputFile = "";
+  string fileName = "";
+  string userInputType;
+  int genCount, numRows, numColumns;
+  genCount = 0;
+  float initialPopulationDensity;
+  //ask the user what type of grid they want
   cout << "Do you want to provide a file map of the world, or would you like a random configuration?" << endl;
   cout << "If providing file map, type 'A' " << endl;
   cout << "If want random assignment, type 'B' " << endl;
-  char userInputType;
+
   cin >> userInputType;
-  //will pass in the txt file and convert it from - and * to 0 and 1
-  if (userInputType == 'A' || userInputType == 'a')
+
+  //cout << userInputType << endl;
+  if (!(toUpperFunction(userInputType) == "A" || toUpperFunction(userInputType) == "B"))
   {
-    string fileName;
+    cout << "INVALID ENTRY FOR " + userInputType << endl;
+    return 0;
+  }
+
+  //asks the mode user wants to run in
+  //if invalid mode terminate the program
+  cout << "What mode do you want to run the matrix in?" << endl;
+  cout << "For Classic Mode, type 'CLASSIC': "<< endl;
+  cout << "For Torus Mode, type 'TORUS' " << endl;
+  cout << "For Mirror Mode, type 'MIRROR' " << endl;
+  string modeType;
+  cin >> modeType;
+  if (!(toUpperFunction(modeType) == "CLASSIC" || toUpperFunction(modeType) == "TORUS" || toUpperFunction(modeType) == "MIRROR"))
+  {
+    cout << "INVALID ENTRY FOR " + modeType << endl;
+    return 0;
+  }
+
+  //asks the user if they want to directly output all generations to a txt files
+  //or if they want to print it to the screen and pause in between each generation
+  cout << "Type 'PAUSE' to pause in between the generations. " << endl;
+  cout << "Or output to a file by typing 'OUTPUT'." << endl;
+  string runCase;
+  cin >> runCase;
+  //if user input is not of the key word, terminate the program
+  if (!(toUpperFunction(runCase) == "PAUSE" || toUpperFunction(runCase) == "OUTPUT"))
+  {
+    cout << "INVALID ENTRY FOR " + runCase << endl;
+    return 0;
+  }
+  if (toUpperFunction(runCase) == "OUTPUT")
+  {
+    cout << "Enter a file name to output to: " << endl;
+    cin >> outputFile;
+  }
+
+  //opens the given txt file and reads in the grid
+  ifstream myfile;
+  if (toUpperFunction(userInputType) == "A")
+  {
     cout << "Enter file name to analyze: " << endl;
     cin >>fileName;
-
-    ifstream myfile;
-    int Rows, Cols;
-    //char **arr;
     myfile.open(fileName);
     if (myfile.good())
     {
-      myfile >> Rows >> Cols;
-      char **arr = new char*[Rows];
-      cout << Rows << endl;
-      cout << Cols << endl;
-      for (int g = 0; g < Rows; g++)
-      {
-        arr[g] = new char[Cols];
-        for (int f = 0; f < Cols; f++)
-        {
-          myfile >> arr[g][f];
-          //if(arr[g][f] == 'X')
-            //arr[g][f] = 1;
-          //cout << arr[g][f] << " ";
-        }
-        //cout << endl;
-      }
-      //copies the char 2d array into an int 2d array
-      //replaces the 'X's with 1's and '-'s with 0's
-      int **grid = new int*[Rows];
-      for (int i = 0; i < Rows; i++)
-      {
-        grid[i] = new int[Cols];
-      }
-      for (int i = 0; i < Rows; i++)
-      {
-        for (int j = 0; j < Cols; j++)
-        {
-          //(int)arr[g][f]  = grid[i][j]
-          if(arr[i][j] == 'X')
-          {
-            grid[i][j] = 1;
-            //cout << arr[i][j] << "is x";
-          }
-          if(arr[i][j] == '-')
-          {
-            grid[i][j] = 0;
-            //cout << arr[i][j] << "is 0";
-          }
-        }
-      }
-      for(int i = 0; i < Rows; i++)
-      {
-        for (int j = 0; j < Cols; j++)
-        {
-          cout << grid[i][j] << " ";
-        }
-        cout << endl;
-      }
-      //myfile.close(fileName);
-      //delete [] arr;
-      //convertCharArray(arr,Rows,Cols);
-      cout << "What mode do you want to run the matrix in?" << endl;
-      cout << "For Classic Mode, type 'CLASSIC': "<< endl;
-      cout << "For Torus Mode, type 'TORUS' " << endl;
-      cout << "For Mirror Mode, type 'MIRROR' " << endl;
-      string modeType;
-      cin >> modeType;
-
-      if (modeType == "CLASSIC")
-      {
-        nextGenerationClassic(grid, Rows, Cols);
-      }
-      else if (modeType == "TORUS")
-      {
-        donutMatrix(grid, Rows, Cols);
-      }
-      else if (modeType == "MIRROR")
-      {
-        mirrorMatrix(grid, Rows, Cols);
-      }
+      myfile >> numRows >> numColumns;
+      cout <<numRows << endl;
+      cout << numColumns << endl;
     }
-
-
-
   }
-
-  if (userInputType == 'B' || userInputType == 'b')
+  //reads in the user data for the random grid
+  else if (toUpperFunction(userInputType) == "B")
   {
-    int numRows, numColumns;
     cout << "Enter the amount of rows you want in the world: " << endl;
     cin >> numRows;
     cout << "Enter the amount of columns you want in the world: " << endl;
     cin >> numColumns;
-    //This will determine how full or empty the randomly generated matrix will be
+    //This will determine how full the randomly generated matrix will be
     cout << "Choose a decimal number (to the hundredth place) between 0 and 1: " << endl;
-    float initialPopulationDensity;
+
     cin >> initialPopulationDensity;
     if(initialPopulationDensity == 0 || initialPopulationDensity == 1)
     {
       cout << "Error" << endl;
       cout << "Enter a valid number: "<< endl;
     }
+  }
 
-    //creates a dynamic 2d array
-    int **a = new int*[numRows];
-    for (int i = 0; i < numRows; ++i)
+  //creates a dynamic 2d array
+  int **grid = new int*[numRows];
+  for (int i = 0; i < numRows; ++i)
+  {
+    grid[i] = new int[numColumns];
+  }
+  for (int i = 0; i < numRows; i++)
+  {
+    for (int j = 0; j < numColumns; j++)
     {
-      a[i] = new int[numColumns];
+      //initializes the 2d array to be full of zeros
+      grid[i][j] = 0;
     }
+  }
 
+  if (toUpperFunction(userInputType) == "A")
+  {
+    //copies the char 2d array into an int 2d array
+    char **arr = new char*[numRows];
+    //read in elements from txt and allocates it to char array
+    for (int g = 0; g < numRows; g++)
+    {
+      arr[g] = new char[numColumns];
+      for (int f = 0; f < numColumns; f++)
+      {
+        myfile >> arr[g][f];
+      }
+    }
+    //converts arr to grid, therefore changing its value
+    //replaces the 'X's with 1's and '-'s with 0's
+    //passes the grid into out txt file or to screen
+    writeMessage("Generation " + to_string(genCount), outputFile);
+    string line= "";
+    for (int i = 0; i < numRows; i++)
+    {
+      grid[i] = new int[numColumns];
+    }
+    for (int i = 0; i < numRows; i++)
+    {
+      for (int j = 0; j < numColumns; j++)
+      {
+        line += arr[i][j];
+        if(arr[i][j] == 'X')
+        {
+          grid[i][j] = 1;
+        }
+        if(arr[i][j] == '-')
+        {
+          grid[i][j] = 0;
+        }
+        //cout << arr[i][j] << " ";
+      }
+      writeMessage(line,outputFile);
+      line = "";
+      //cout << endl;
+    }
+  }
+
+  else if (toUpperFunction(userInputType) == "B")
+  {
     //to find amount of full cells in the user generated matrix
     int amountFullCells;
     amountFullCells = (numRows * numColumns) * initialPopulationDensity;
@@ -355,16 +498,6 @@ int main(/*int argc, char **argv*/)
     cout << numColumns << endl;
 
     srand((unsigned)time(NULL));
-    //creates the matrix with the given input to form the size
-    //initializes the matrix with 0, representing empty cells
-    //will use the initialPopulationDensity to determine how many Xs will be randomly initialized
-    for (int i = 0; i < numRows; i++)
-    {
-      for (int j = 0; j < numColumns; j++)
-      {
-        a[i][j] = 0;
-      }
-    }
 
     //loop to randomly plot the full cells (X)
     int plottedFullCells = 0;
@@ -381,7 +514,7 @@ int main(/*int argc, char **argv*/)
         {
           if (i == x && j == y)
           {
-            if (a[i][j] == 1)
+            if (grid[i][j] == 1)
             {
               is_plotted = 1;
               break;
@@ -392,44 +525,73 @@ int main(/*int argc, char **argv*/)
       //plots the X at randomly generated element
       if(is_plotted == 0)
       {
-          a[x][y] = 1;
+          grid[x][y] = 1;
       }
       plottedFullCells++;
     }
 
+    writeMessage("Generation: " + to_string(genCount), outputFile);
+    string line= "";
     for (int i = 0; i < numRows; i++)
     {
       for (int j = 0; j < numColumns; j++)
       {
-        cout << a[i][j] << " ";
+        if(grid[i][j] == 0)
+          line +=  "-";
+        else
+          line += "X";
       }
       cout << endl;
+      writeMessage(line,outputFile);
+      line = "";
     }
-    /*for(int i = 0; i < numRows; ++i)
-    {
-      delete [] a[i];
-    }
-    */
-    cout << "What mode do you want to run the matrix in?" << endl;
-    cout << "For Classic Mode, type 'CLASSIC': "<< endl;
-    cout << "For Torus Mode, type 'TORUS' " << endl;
-    cout << "For Mirror Mode, type 'MIRROR' " << endl;
-    string modeType;
-    cin >> modeType;
+  }
+  //takes the return grids and copies them to be able to be used in main
+  int **donutGrid = donutMatrix(grid, numRows, numColumns, outputFile);
+  int ** mirrorGrid = mirrorMatrix(grid, numRows, numColumns, outputFile);
 
-    if (modeType == "CLASSIC")
+
+  //loops through the functions to create the future generations
+  int isStable = 0;
+  while(isStable == 0)
+  {
+    genCount++;
+    string msg = (string)"Generation: " + to_string(genCount);
+    writeMessage(msg, outputFile);
+    if (toUpperFunction(modeType) == "CLASSIC")
     {
-      nextGenerationClassic(a, numRows, numColumns);
+      //passes the original generation into nextGenerationClassic
+      grid = nextGenerationClassic(grid, numRows, numColumns, outputFile);
+      //checks to see if the grid is full of zeros or ones
+      //if not, proceed with the loop
+      isStable = testIfStable(grid, genCount, numRows,numColumns);
     }
-    else if (modeType == "TORUS")
+    else if (toUpperFunction(modeType) == "TORUS")
     {
-      donutMatrix(a, numRows, numColumns);
+      if (genCount > 1)
+      {
+        donutGrid = donutMatrix(donutGrid, numRows, numColumns, outputFile);
+      }
+      //passes in the original generation of the augmented grid to nextGenerationAugmented
+      donutGrid = nextGenerationAugmented(donutGrid, numRows, numColumns, outputFile);
+      isStable = testIfStableAug(donutGrid, genCount, numRows, numColumns);
     }
-    else if (modeType == "MIRROR")
+    else if (toUpperFunction(modeType) == "MIRROR")
     {
-      mirrorMatrix(a, numRows, numColumns);
+      if (genCount > 1)
+      {
+        mirrorGrid = mirrorMatrix(mirrorGrid, numRows, numColumns, outputFile);
+      }
+      //passes in the original generation of the augmented grid to nextGenerationAugmented
+      mirrorGrid = nextGenerationAugmented(mirrorGrid, numRows, numColumns, outputFile);
+      isStable = testIfStableAug(mirrorGrid, genCount, numRows, numColumns);
     }
-    //delete [] a;
-  } //end of if statement where user inputs their own data
+    //pauses the while loop for 5 seconds if the user wants to view each generation
+    if(toUpperFunction(runCase) == "PAUSE")
+    {
+      //system("pause") would not work so using sleep() instead
+      sleep(5);
+    }
+  }
   return 0;
 }
